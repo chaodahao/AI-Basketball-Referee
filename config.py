9059bf4c09b54b1fba87819162cd23f57ceb1589
ChenWeilinx <2735378542@qq.com>
2024-03-05 17:38:46 +0800

    check given files to ensure thay exist

--- config.py
+++ config.py
@@ -1,8 +1,38 @@
+import os
+
 BALL_MODEL_PATH = "./lib/yolov5/checkpoint/yolov5s_basketball.pt"
 # BALL_MODEL_PATH = "./checkpoints/basketballModel.pt"
 POSE_MODEL_PATH = "./checkpoints/yolov8s-pose.pt"
 
-VIDEO = "./videos/.mp4"
+VIDEO = "./videos/c.mp4"
 SAVE_BASE_PATH = "./travel_footage"
 
 SAVING = True
+
+def check_files(file_list):
+    """
+    Check the existence of files in the given list.
+    If any file in the list does not exist, an AssertionError is raised with a corresponding error message.
+
+    Parameters:
+    file_list (list): A list of file paths to be checked.
+    """
+    
+    for f in file_list:
+        assert os.path.exists(f), f"{f} do not exists, please check whether the file name or file path given is correct"
+        print(f"{f} exists")
+    
+def mk_dir(file_list):
+    """
+    Create directories in the given list if they do not already exist.
+
+    Parameters:
+    file_list (list): A list of directory paths to be created.
+    """
+    for f in file_list:
+        if not os.path.exists(f):
+            print(f"Directory:{f} do not exists, already created!")
+            os.mkdir(f)
+
+check_files([BALL_MODEL_PATH, POSE_MODEL_PATH, VIDEO])
+mk_dir([SAVE_BASE_PATH])
